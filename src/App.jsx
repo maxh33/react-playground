8dece82e6e23eee81e65010f676688b7a08e20c4
maxh33 <contact@maxhaider.dev>
2024-11-12 10:31:29 -0300

    feat error 404 handle

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Perfil from "./components/Perfil";
-import Formulario from './components/Formulario';
+// import Formulario from './components/Formulario';
 import ReposList from './components/ReposList';
 
 
 function App() {
-  const [formulatioEstaVisivel, setFormulatioEstaVisivel] = useState(true)
+  // const [formulatioEstaVisivel, setFormulatioEstaVisivel] = useState(true)
   const [nomeUsuario, setNomeUsuario] = useState('')
   return (
     <>
